2fea187deedf221cdb5ca52a8d9b2663c1a38688
pasha <pavlo.zalevsky@gmail.com>
2016-04-26 13:45:59 +0300

    First progect

---
+++ FirstASP.NETprogect/FirstASP.NETprogect/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstASP.NETprogect.Models;
+
+namespace FirstASP.NETprogect.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 10 ? "Доброе утро" : "Доброго дня";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+            {
+                // Нужно отправить данные нового гостя no электронной почте 
+                // организатору вечеринки.
+                return View("Thanks", guest);
+            }
+            else
+                // Обнаружена ошибка проверки достоверности
+                return View();
+
+        }
+    }
+}
