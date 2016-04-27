50cff38eae48634217a56397851e930559096472
pasha <pavlo.zalevsky@gmail.com>
2016-04-27 18:19:56 +0300

    ADD new progect

---
+++ EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product[] products = {
+            new Product {Name = "Каяк", Category = "Водные виды спорта", Price = 275M},
+            new Product {Name = "Спасательный жилет", Category = "Водные виды спорта", Price = 48.95M},
+            new Product {Name = "Мяч", Category = "Футбол", Price = 19.50M},
+            new Product {Name = "Угловой флажок", Category = "Футбол", Price = 34.95M}
+        };
+
+        public ActionResult Index()
+        {
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+
+            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+
+            ShoppingCart cart = new ShoppingCart(calc)
+            {
+                Products = products
+            };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+    }
+}
