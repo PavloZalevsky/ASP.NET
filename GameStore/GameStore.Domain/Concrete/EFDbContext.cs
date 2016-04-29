2691ac082bd24628c760d63b0474130055997901
pasha <pavlo.zalevsky@gmail.com>
2016-04-29 18:06:37 +0300

    entity =(

--- GameStore/GameStore.Domain/Concrete/EFDbContext.cs
+++ GameStore/GameStore.Domain/Concrete/EFDbContext.cs
@@ -1,10 +1,12 @@
-﻿using GameStore.Domain.Entities;
-using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace GameStore.Domain.Concrete
 {
-    public class EFDbContext : DbContext
+    class EFDbContext
     {
-        public DbSet<Game> Games { get; set; }
     }
 }
