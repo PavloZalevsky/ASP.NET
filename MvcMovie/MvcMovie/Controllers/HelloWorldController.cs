8d13b86cb169f358d75a76dcffd0fdc15314fde7
PavloZalevsky <pavlo.zalevsky@gmail.com>
2016-05-10 18:14:16 +0300

    ADD: 3 Lessons

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -8,17 +8,19 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
 
         // 
-        // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome(string name = "xx", int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
+
     }
 }
