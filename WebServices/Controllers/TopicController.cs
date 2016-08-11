318f6bb984d119ec2dc5068da8ee8875e9a8b974
PavloZalevsky <pavlo.zalevsky@gmail.com>
2016-08-11 21:29:26 +0300

    ADD: project wep api

---
+++ WebServices/Controllers/TopicController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class TopicController : ApiController
+    {
+        public ICollection<Topic> Get()
+        {
+            return new Collection<Topic>
+                    {
+                        new Topic { Id = 1, Title = "Топик 1"},
+                        new Topic { Id = 2, Title = "Топик 2"}
+                    };
+        }
+    }
+}
